217eeb929575ab18b154b02faa288e60960cb506
Amruth Venkatraman <amruthvenkatraman@Amruths-MacBook-Air.local>
2014-02-17 22:54:03 -0500

    testing serial working

---
+++ testSerial/testSerial.ino
@@ -0,0 +1,22 @@
+#define LED_PIN 13
+
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  while (!Serial) {} //wait for connection 
+  Serial.print(21);
+}
+
+void loop()                       // run over and over again
+{
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+  //  analogWrite(3, inByte);
+    Serial.print("char read: " + inByte);
+  } 
+}
+
+void toggleLED() {
+  Serial.println("in toggle");
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+}
