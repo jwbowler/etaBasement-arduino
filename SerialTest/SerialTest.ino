5b0034fea2ac0dd0c9e268807c7a84da44e6115a
Amruth V <amruthv@amruthv.(none)>
2014-02-28 00:32:02 -0500

    zbt pin writes

--- SerialTest/SerialTest.ino
+++ SerialTest/SerialTest.ino
@@ -1,20 +1,34 @@
 #include<stdio.h>
 
 #define LED_PIN 13
-#define R_PIN 3
-#define G_PIN 5
-#define B_PIN 6
+#define R_B_PIN 8
+#define G_B_PIN 9
+#define B_B_PIN 10
+#define R_M_PIN 5
+#define G_M_PIN 6
+#define B_M_PIN 7
+#define R_T_PIN 2
+#define G_T_PIN 3
+#define B_T_PIN 4
 #define frameBegin ((byte) 255)
 
 byte colors[] = {
   0,0,0};
+  
+boolean allThree = true;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
-  pinMode(R_PIN, OUTPUT);
-  pinMode(G_PIN, OUTPUT);
-  pinMode(B_PIN, OUTPUT);
+  pinMode(R_B_PIN, OUTPUT);
+  pinMode(G_B_PIN, OUTPUT);
+  pinMode(B_B_PIN, OUTPUT);
+  pinMode(R_M_PIN, OUTPUT);
+  pinMode(G_M_PIN, OUTPUT);
+  pinMode(B_M_PIN, OUTPUT);
+  pinMode(R_T_PIN, OUTPUT);
+  pinMode(G_T_PIN, OUTPUT);
+  pinMode(B_T_PIN, OUTPUT);
   //  establishContact();
   Serial.println("blah");
 }
@@ -26,24 +40,58 @@ void loop() {
     
     if (inByte == frameBegin) {
       
-      //boolean allThree = true;
+
+      allThree = true;
+      //GET BASS
       for (int i=0; i < 3; i++) {
         while (!Serial.available()) { }
         byte rgb = Serial.read();
         if (rgb == frameBegin) {
-          //Serial.println('F');
-          //allThree = false;
-          //break;
-          return;
+          allThree = false;
+          break;
+        }
+        else {
+          colors[i] = rgb;
+        }
+      }
+      analogWrite(R_B_PIN, colors[0]);
+      analogWrite(G_B_PIN, colors[1]);
+      analogWrite(B_B_PIN, colors[2]);
+      
+      //GET MID
+      allThree = true;
+      for (int i=0; i < 3; i++) {
+        while (!Serial.available()) { }
+        byte rgb = Serial.read();
+        if (rgb == frameBegin) {
+          allThree = false;
+          break;
+        }
+        else {
+          colors[i] = rgb;
+        }
+      }
+      analogWrite(R_M_PIN, colors[0]);
+      analogWrite(G_M_PIN, colors[1]);
+      analogWrite(B_M_PIN, colors[2]);
+      
+      //GET Treble
+      allThree = true;
+      for (int i=0; i < 3; i++) {
+        while (!Serial.available()) { }
+        byte rgb = Serial.read();
+        if (rgb == frameBegin) {
+          allThree = false;
+          break;
         }
         else {
           colors[i] = rgb;
         }
       }
       //printRGB();
-      analogWrite(R_PIN, colors[0]);
-      analogWrite(G_PIN, colors[1]);
-      analogWrite(B_PIN, colors[2]);
+      analogWrite(R_T_PIN, colors[0]);
+      analogWrite(G_T_PIN, colors[1]);
+      analogWrite(B_T_PIN, colors[2]);
     }
   }
   delay(1);
